7fc4d2f2992755d0d57c57be1662661c70a247ef
Marcelo Sekimoto <msekimoto@gmail.com>
2023-09-14 02:56:35 -0300

    valor de cidade dinamico

--- teste.js
+++ teste.js
@@ -1,7 +1,74 @@
 
 function httpGet() {
+
+    if (navigator.geolocation) {
+        return navigator.geolocation.getCurrentPosition(showPosition);
+      } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+    
+    function showPosition(position) 
+    {
+            
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "https://api-traveller.azurewebsites.net/Hoteis/PorNome?cidade=sa", false); // false for synchronous request
+    
+    let url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude +"&key=AIzaSyDvchNONgwXYd119ZmjJKEZKhHuiTbR8zQ"
+    
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+
+    let data = JSON.parse(xmlHttp.responseText);
+    let results = data.results;
+
+    var country = null, countryCode = null, city = null, cityAlt = null;
+    var c, lc, component;
+    for (var r = 0, rl = results.length; r < rl; r += 1) {
+        var result = results[r];
+
+        if (!city && result.types[0] === 'locality') {
+            for (c = 0, lc = result.address_components.length; c < lc; c += 1) {
+                component = result.address_components[c];
+
+                if (component.types[0] === 'locality') {
+                    city = component.long_name;
+                    break;
+                }
+            }
+        }
+        else if (!city && !cityAlt && result.types[0] === 'administrative_area_level_2') {
+            for (c = 0, lc = result.address_components.length; c < lc; c += 1) {
+                component = result.address_components[c];
+
+                if (component.types[0] === 'administrative_area_level_1') {
+                    cityAlt = component.long_name;
+                    break;
+                }
+            }
+        } else if (!country && result.types[0] === 'country') {
+            country = result.address_components[0].long_name;
+            countryCode = result.address_components[0].short_name;
+        }
+
+        if (city && country) {
+            break;
+        }
+
+        
+    }
+
+    //   console.log("City: " + city + ", Country: " + country + ", Country Code: " + countryCode);
+    let location = { 
+        "city": city,
+        "country" : country,
+        "countryCode": countryCode
+    }
+            
+
+    console.log(location);
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "https://api-traveller.azurewebsites.net/Hoteis/PorNome?cidade=" + location.city, false); // false for synchronous request
     xmlHttp.send(null);
     console.log(xmlHttp.responseText);
 
