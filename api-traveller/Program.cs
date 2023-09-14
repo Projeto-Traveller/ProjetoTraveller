b18d45ec6a264b63d26bc3671fa1b259417f2dc9
Marcelo Sekimoto <msekimoto@gmail.com>
2023-09-14 16:26:47 -0300

    enable prod swagger

--- api-traveller/Program.cs
+++ api-traveller/Program.cs
@@ -33,11 +33,14 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
